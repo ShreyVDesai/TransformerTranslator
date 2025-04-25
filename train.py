7058bbb7ef719dd6dc131a4d6cabed5df00739bc
ShreyVDesai <shreyvdesai@gmail.com>
2025-04-25 12:13:24 -0400

    get_ds done

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizer.pre_tokenizers import Whitespace
 from pathlib import Path
-
+from dataset import BilingualDataset, causal_mask
 
 def get_all_sentences(ds, lang):
     for item in ds:
@@ -40,5 +40,22 @@ def get_ds(config):
 
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_raw,val_ds_raw])
 
-    
+    train_ds = BilingualDataset(train_ds_raw,tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw,tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
 
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Max len of src language : {max_len_src}')
+    print(f'Max len of tgt language : {max_len_tgt}')
+
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle = True)
+    val_dataloader = DataLoader(val_ds, batch_size = 1, shuffle = True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
